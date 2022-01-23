22005672daf34815b45f4beee70703879898e977
Hesham <heshamelmasry1992@gmail.com>
2022-01-23 20:22:17 +0100

    Add the questions for lesson 2

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// We code it together
+
+// 1. Select heading h2
+
+// 2. Select element with class .heading2
+
+// 3. Select element with id #list using querySelector
+
+// 4. Select element with id #list using getElementById
+
+// 5. Select single li
+
+// 6.
+//  a. Select Multiple li's
+//  b. Loop on the list items
+
+// 7.
+    // a. select h1 element
+    // b. Change the color of that h1 to be purple
+    // c. Change the border to be 1px solid green
+    // d. Change background colour to be light gray
+    // e. Change the padding to be 1em ( remember that 1em is the base in px you put on the body )
+
+
